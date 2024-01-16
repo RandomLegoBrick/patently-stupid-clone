b73de02f02224d5ae175e465a389d6bc17ef4457
ZeRedBarron <paulinobarron85@outlook.com>
2024-01-15 17:41:03 -0800

    liveshare update

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+//const socket = io();
+
 var bdy = window.document.querySelector("body");
 
 var clicked = false;
@@ -10,16 +12,24 @@ bdy.addEventListener("mouseup", function() {
 
 var drawpad = document.getElementById("drawpad");
 var drawpadCTX = drawpad.getContext("2d");
-drawpadCTX.sharpie = function(x, y){
-    this.fillStyle = "rgb(0, 0, 0)"
-}
+drawpadCTX.sharpiePoint = function(x, y){
+    this.fillStyle = "rgb(0, 0, 0)";
+    this.strokeStyle = "rgb(0, 0, 0)";
 
+    this.arc(x, y, 3, 0, 360);
+}
 
 var drawpadPoints = [];
 
-drawpad.addEventListener("mousemove", function(){
+drawpad.addEventListener("mousemove", function(e){
     if(clicked) {
-        drawpadPoints.push({x: clientX, y: clientY});
+        drawpadPoints.push({x: e.clientX, y: e.clientY});
+        drawpadCTX.sharpiePoint(e.clientX, e.clientY);
+        console.log("drawing")
     }
 });
 
+drawpadCTX.sharpiePoint(20, 20);
+
+drawpadCTX.fillStyle = "rgb(0, 0, 0)";
+//drawpadCTX.
