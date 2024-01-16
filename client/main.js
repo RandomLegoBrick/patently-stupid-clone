1ccd29ccdedfe6e4545ec24218de68bf305c9469
ZeRedBarron <paulinobarron85@outlook.com>
2024-01-15 18:22:19 -0800

    Drawpad sorta working

--- client/main.js
+++ client/main.js
@@ -27,15 +27,23 @@ drawpad.addEventListener("mousemove", function(e){
     if(clicked) {
         drawpadPoints.push({x: e.clientX, y: e.clientY});
         console.log("drawing")
+
+        drawpadCTX.strokeStyle = "rgb(0, 0, 0)"
+        drawpadCTX.beginPath();
+        drawpadCTX.moveTo(drawpadPoints.x, drawpadPoints.y);
+        for(var i = 0; i < drawpadPoints.length; i++) {
+            drawpadCTX.lineTo(drawpadPoints[i].x, drawpadPoints[i].y);
+            drawpadCTX.lineWidth = 3;
+            drawpadCTX.stroke();
+        }
     }
 });
 
 drawpadCTX.sharpiePoint(20, 20);
 
-drawpadCTX.strokeStyle = "rgb(0, 0, 0)"
-drawpadCTX.beginPath();
-for(var i = 0; i < drawpadPoints.length; i++) {
-    drawpadCTX.lineTo(drawpadPoints[i].x, drawpadPoints[i].y);
-    drawpadCTX.lineWidth = 3;
-    drawpadCTX.stroke();
-}
+
+
+drawpadCTX.beginPath(); // Start a new path
+drawpadCTX.moveTo(30, 50); // Move the pen to (30, 50)
+drawpadCTX.lineTo(100, 100); // Draw a line to (150, 100)
+drawpadCTX.stroke(); // Render the path
